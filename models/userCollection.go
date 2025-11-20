b8a049478ca1b8af293203c0aade3508de82f082
bleach <142779648+b1each232425@users.noreply.github.com>
2025-11-20 09:29:41 +0800

    添加用户收藏表

---
+++ models/userCollection.go
@@ -0,0 +1,7 @@
+package models
+
+type userCollection struct {
+	BaseModel
+	TeamID string `json:"team_id" gorm:"type:varchar(64);not null;comment:队伍ID;index:idx_user_team,unique"`
+	UserID string `json:"user_id" gorm:"type:varchar(64);not null;comment:用户业务ID;index:idx_user_team,unique"`
+}
