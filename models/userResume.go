5ebf7c48e528b3116abc4d82cba6832d874ca98b
bleach <142779648+b1each232425@users.noreply.github.com>
2025-11-21 08:44:37 +0800

    添加简历models

--- models/userResume.go
+++ models/userResume.go
@@ -6,4 +6,5 @@ type userResume struct {
 	ResumeID   string `json:"resume_id gorm:type:varchar(64);not null;comment:简历ID"`
 	ResumeName string `json:"resume_name gorm:type:varchar(255);not null comment:简历名"`
 	FilePath   string `json:"file_path gorm:type:text;not null comment:文件路径"`
+	FileType   string `json:"file_type gorm:type:varchar(64);not null comment:文件类型"`
 }
