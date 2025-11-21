3b07baf5d5aa8e36694e43b687a66b953b1f50e5
yzh <3370157161@qq.com>
2025-11-21 08:41:48 +0800

    新增登录相关接口

---
+++ utils/jwt.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"time"
+	"zhq-backend/config"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// Claims 自定义claims
+type Claims struct {
+	UserID uint   `json:"user_id"`
+	OpenID string `json:"open_id"`
+	jwt.RegisteredClaims
+}
+
+// GenerateToken 生成JWT令牌
+func GenerateToken(userID uint, openID string) (string, error) {
+	//设置过期时间为7天
+	expirationTime := time.Now().Add(7 * 24 * time.Hour)
+
+	claims := &Claims{
+		UserID: userID,
+		OpenID: openID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(config.GetString("jwt.secret")))
+}
+
+// ParseToken 验证JWT令牌
+func ParseToken(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.GetString("jwt.secret")), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, jwt.ErrTokenInvalidClaims
+}
